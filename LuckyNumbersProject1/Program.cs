24e290fbd806758c252899e47b230801ba553916
HopeMorehead <splendidwonder@gmail.com>
2017-07-09 01:44:18 -0400

    Mathamatics

--- LuckyNumbersProject1/Program.cs
+++ LuckyNumbersProject1/Program.cs
@@ -15,7 +15,7 @@ namespace LuckyNumbersProject1
             int jackPot = 63;
             int i;
             int a;
- //         int Tempval = answer[i];
+            //int Tempval = answer[i];
 
             Console.WriteLine("Pick a low start number.");
             int minValue = int.Parse(Console.ReadLine());
@@ -24,7 +24,7 @@ namespace LuckyNumbersProject1
 
 
             int[] answer = new int[6];
-            for ( i = 0; i < answer.Length; i++)
+            for (i = 0; i < answer.Length; i++)
             {
                 Console.WriteLine($"Please enter answer 6 numbers");
                 answer[i] = int.Parse(Console.ReadLine());
@@ -39,7 +39,7 @@ namespace LuckyNumbersProject1
                 }
 
             }
-  
+
 
 
 
@@ -52,38 +52,33 @@ namespace LuckyNumbersProject1
                 winnings[a] = random.Next(0, winnings.Length - 1);
                 Console.WriteLine("Lucky Numbers :" + winnings[a]);
             }
-
-            if (answer[i - 1] == winnings[a - 1]) 
-
+         //   bool result;
+            foreach (int number in answer)
             {
-                Console.WriteLine("You won");
+                int count = 0;
+                foreach (int number2 in winnings)
+                {
+                    if (number2 == number)
+                    {
+                        count = count + 1;
+
+                        Console.WriteLine("\t\n " + number + " occurs " + count + " times");
+                    }
+                }
+                //    Console.WriteLine(number == number2);
+                // result = true;
+                //  Console.WriteLine("number{0}",number==number2);
+
             }
-           //  count1 = count1 + 1;
-//    Console.WriteLine("\t\n " + answer + " occurs " + count + " times");
-//}
-//   Console.WriteLine(Array.IndexOf(answer, “October”));
-
-
-
+            
         }
 
     }
 }
 
 
-/*   Console.WriteLine($"The JackPot numer is {jackPot}:");
-   if (i == jackPot)
-   {
 
-       Console.WriteLine("You have won some funds!");
-
-   }*/
-
-
-
-
-
-//  int Tempval2 = winnings[a];
-
-// 
+//    Console.WriteLine("\t\n " + answer + " occurs " + count + " times");
+//}
+//   Console.WriteLine(Array.IndexOf(answer, “October”));
 
